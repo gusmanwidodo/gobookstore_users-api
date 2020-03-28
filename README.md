a743cbd417fdecbdec3e592dd53f6c6135ef0768
Gusman Widodo <gusmanwidodo@gmail.com>
2020-03-28 20:35:31 +0800

    Add MVC architecture/skeleton

---
+++ README.md
@@ -0,0 +1,2 @@
+# BOOK STORE: USERS API
+A microservice written in Go to store and provide users data of a book store application.
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gusmanwidodo/gobookstore_users-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
