3f1b1868cbd5a93ca8deaca59430ad56b2279c2b
Gusman Widodo <gusmanwidodo@gmail.com>
2020-03-29 11:08:45 +0800

    Introduce data access object (DAO)

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,6 +2,7 @@ package users
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gusmanwidodo/gobookstore_users-api/domain/users"
@@ -12,7 +13,7 @@ import (
 func CreateUser(c *gin.Context) {
 	var user users.User
 	if err := c.ShouldBindJSON(&user); err != nil {
-		restErr := errors.NewBadRequestErr("invalid json body")
+		restErr := errors.NewBadRequestError("invalid json body")
 		c.JSON(restErr.Status, restErr)
 		return
 	}
@@ -27,5 +28,18 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
--- utils/errors/rest_error.go
+++ utils/errors/rest_error.go
@@ -3,15 +3,23 @@ package errors
 import "net/http"
 
 type RestErr struct {
-	Message	string	`json:"message"`
-	Status	int	`json:"status"`
-	Error	string	`json:"error"`
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
 }
 
-func NewBadRequestErr(message string) *RestErr {
+func NewBadRequestError(message string) *RestErr {
 	return &RestErr{
 		Message: message,
-		Status: http.StatusBadRequest,
-		Error: "bad_request",
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
 	}
 }
